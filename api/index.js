e0a613ddb5f3f05e44966f7baaa5193475fd430e
shrish <shrish.dollin1@gmail.com>
2023-08-21 18:50:51 +0530

    upload from device & perks fixed

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const imageDownloader = require("image-downloader")
+const multer = require('multer');
+const fs = require('fs');
 require("dotenv").config();
 const app = express();
 
@@ -136,6 +138,22 @@ app.post('/upload-by-link', async (req, res) => {
       dest: __dirname + '/uploads/' + newName,
     });
     res.json(newName);  
+});
+
+const photosMiddleware = multer({dest: 'uploads'});
+
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    // const fileInfo = req.files[i];
+    const {path, originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads/', ''));
+  }
+  res.json(uploadedFiles);
 });
 
 // connectDB();
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -44,6 +44,23 @@ export default function PlacesPage() {
     setPhotoLink('');
   }
 
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
+    // console.log({files});
+    const data = new FormData();
+    for(let i = 0; i < files.length; i++) {
+      data.append('photos', files[i]);
+    }
+    axios.post('/upload', data, {
+      headers: {'Content-Type': 'multipart/form-data'}
+    }).then(response => {
+      const {data: filenames} = response;
+      setAddedPhotos(prev => {
+        return [...prev, ...filenames];
+      });
+    });
+  }
+
   return (
     <div>
       <div className="text-center">
@@ -112,11 +129,12 @@ export default function PlacesPage() {
             <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhotos.length > 0 && addedPhotos.map(link => (
                 // eslint-disable-next-line react/jsx-key
-                <div>
-                  <img className="rounded-2xl" src={'http://localhost:4000/uploads/' + link} alt="" />
+                <div className="h-32 flex" key={link}>
+                  <img className="rounded-2xl w-full object-cover" src={'http://localhost:4000/uploads/' + link} alt="" />
                 </div>
               ))}
-              <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-xl text-gray-600">
+              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-xl text-gray-600">
+                <input type="file" multiple className="hidden" onChange={uploadPhoto} />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -132,7 +150,7 @@ export default function PlacesPage() {
                   />
                 </svg>
                 Upload
-              </button>
+              </label>
             </div>
             {/* Description */}
             {preInput(
